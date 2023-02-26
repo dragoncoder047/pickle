7954045ff9b5dce745485d4bee47aa1c0bd3105b
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-02-26 14:01:49 -0500

    `pk_` --> `pik_`

--- pickle.h
+++ pickle.h
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <assert.h>
 
-#define PK_DEBUG
+#define PIK_DEBUG
 
 #ifdef __cplusplus
 extern "C" {
@@ -18,68 +18,68 @@ typedef uint8_t bool;
 #define false 0
 #endif
 
-typedef uint16_t pk_type;
-typedef uint16_t pk_flags;
-typedef uint16_t pk_resultcode;
-typedef struct pk_object pk_object;
-typedef struct pk_vm pk_vm;
+typedef uint16_t pik_type;
+typedef uint16_t pik_flags;
+typedef uint16_t pik_resultcode;
+typedef struct pik_object pik_object;
+typedef struct pik_vm pik_vm;
 
-typedef pk_object* (*pk_builtin_function)(pk_vm*, pk_object*, size_t, pk_object**);
+typedef pik_object* (*pik_builtin_function)(pik_vm*, pik_object*, size_t, pik_object**);
 
 // These function only operate on the void* payload of the object, everything else is handles automatically
-typedef void (*pk_type_function)(pk_vm*, pk_object*);
-#define PK_NUMTYPEFUNS 3
-#define PK_INITFUN 0
-#define PK_MARKFUN 1
-#define PK_FINALFUN 2
+typedef void (*pik_type_function)(pik_vm*, pik_object*);
+#define PIK_NUMTYPEFUNS 3
+#define PIK_INITFUN 0
+#define PIK_MARKFUN 1
+#define PIK_FINALFUN 2
 
-typedef void (*pk_exit_callback)(pk_vm*, pk_object*);
-typedef void (*pk_write_callback)(pk_vm*, const char*);
-typedef char* (*pk_read_callback)(pk_vm*, const char*);
-typedef char* (*pk_source_callback)(pk_vm*, const char*);
-typedef void (*pk_store_callback)(pk_vm*, const char*, const char*);
-typedef void (*pk_error_callback)(pk_vm*, size_t, const char*);
-typedef const char* (*pk_embeddedfilter_callback)(pk_vm*, const char*);
-typedef const char* (*pk_checkinterrupt_callback)(pk_vm*);
+typedef void (*pik_exit_callback)(pik_vm*, pik_object*);
+typedef void (*pik_write_callback)(pik_vm*, const char*);
+typedef char* (*pik_read_callback)(pik_vm*, const char*);
+typedef char* (*pik_source_callback)(pik_vm*, const char*);
+typedef void (*pik_store_callback)(pik_vm*, const char*, const char*);
+typedef void (*pik_error_callback)(pik_vm*, size_t, const char*);
+typedef const char* (*pik_embeddedfilter_callback)(pik_vm*, const char*);
+typedef const char* (*pik_checkinterrupt_callback)(pik_vm*);
 
 #define streq(a, b) (!strcmp(a, b))
 
-#define PK_TYPE_TOMBSTONE UINT16_MAX
-#define PK_TYPE_NONE 0
+#define PIK_TYPE_TOMBSTONE UINT16_MAX
+#define PIK_TYPE_NONE 0
 
-#define PK_HASHMAP_BUCKETS 256
-#define PK_HASHMAP_BUCKETMASK 0xFF
-typedef struct pk_hashentry {
+#define PIK_HASHMAP_BUCKETS 256
+#define PIK_HASHMAP_BUCKETMASK 0xFF
+typedef struct pik_hashentry {
     char* key;
-    pk_object* value;
+    pik_object* value;
     bool readonly;
-} pk_hashentry;
+} pik_hashentry;
 
-typedef struct pk_hashbucket {
-    pk_hashentry* entries;
+typedef struct pik_hashbucket {
+    pik_hashentry* entries;
     size_t cap;
-} pk_hashbucket;
+} pik_hashbucket;
 
-typedef struct pk_hashmap {
-    pk_hashbucket buckets[PK_HASHMAP_BUCKETS];
-} pk_hashmap;
+typedef struct pik_hashmap {
+    pik_hashbucket buckets[PIK_HASHMAP_BUCKETS];
+} pik_hashmap;
 
-struct pk_object {
-    pk_type type;
+struct pik_object {
+    pik_type type;
     struct {
-        pk_object* next;
+        pik_object* next;
         size_t refcnt;
     } gc;
     struct {
-        pk_flags global;
-        pk_flags obj;
+        pik_flags global;
+        pik_flags obj;
     } flags;
     struct {
-        pk_object** bases;
+        pik_object** bases;
         size_t cap;
         size_t len;
     } proto;
-    pk_hashmap* properties;
+    pik_hashmap* properties;
     union {
         void* as_pointer;
         struct {
@@ -95,21 +95,21 @@ struct pk_object {
     } payload;
 };
 
-typedef struct pk_typemgr {
-    pk_type_function funs[PK_NUMTYPEFUNS];
-    pk_type type_for;
+typedef struct pik_typemgr {
+    pik_type_function funs[PIK_NUMTYPEFUNS];
+    pik_type type_for;
     const char* type_string;
-} pk_typemgr;
+} pik_typemgr;
 
-typedef struct pk_operator {
+typedef struct pik_operator {
     const char* symbol;
     const char* method;
     int precedence;
-} pk_operator;
+} pik_operator;
 
-typedef struct pk_parser pk_parser;
-struct pk_parser {
-    pk_parser* parent;
+typedef struct pik_parser pik_parser;
+struct pik_parser {
+    pik_parser* parent;
     const char* code;
     size_t len;
     size_t head;
@@ -117,51 +117,51 @@ struct pk_parser {
     size_t depth;
 };
 
-struct pk_vm {
+struct pik_vm {
     struct {
-        pk_object* first;
-        pk_object* tombstones;
+        pik_object* first;
+        pik_object* tombstones;
         size_t num_objects;
     } gc;
     struct {
-        pk_typemgr* mgrs;
+        pik_typemgr* mgrs;
         size_t len;
         size_t cap;
     } type_managers;
     struct {
-        pk_operator* ops;
+        pik_operator* ops;
         size_t len;
         size_t cap;
     } operators;
-    pk_parser* parser;
-    pk_object* global_scope;
-    pk_object* dollar_function;
+    pik_parser* parser;
+    pik_object* global_scope;
+    pik_object* dollar_function;
     struct {
-        pk_exit_callback exit;
-        pk_write_callback write;
-        pk_read_callback read;
-        pk_source_callback source;
-        pk_store_callback store;
-        pk_error_callback error;
-        pk_embeddedfilter_callback embeddedfilter;
-        pk_checkinterrupt_callback checkinterrupt;
+        pik_exit_callback exit;
+        pik_write_callback write;
+        pik_read_callback read;
+        pik_source_callback source;
+        pik_store_callback store;
+        pik_error_callback error;
+        pik_embeddedfilter_callback embeddedfilter;
+        pik_checkinterrupt_callback checkinterrupt;
     } callbacks;
 };
 
-#define PK_DOCALLBACK(vm, name, ...) \
+#define PIK_DOCALLBACK(vm, name, ...) \
 if (vm->callbacks.name != NULL) { \
     vm->callbacks.name(__VA_ARGS__);\
 }
 
 // Forward references
-void pk_register_globals(pk_vm*);
-void pk_hashmap_destroy(pk_vm*, pk_hashmap*);
-void pk_decref(pk_vm*, pk_object*);
-inline pk_hashmap* pk_hashmap_new(void);
+void pik_register_globals(pik_vm*);
+void pik_hashmap_destroy(pik_vm*, pik_hashmap*);
+void pik_decref(pik_vm*, pik_object*);
+inline pik_hashmap* pik_hashmap_new(void);
 
 // ------------------- Alloc/dealloc objects -------------------------
     
-void pk_run_typefun(pk_vm* vm, pk_type type, pk_object* object, int name) {
+void pik_run_typefun(pik_vm* vm, pik_type type, pik_object* object, int name) {
     for (size_t i = 0; i < vm->type_managers.len; i++) { 
         if (vm->type_managers.mgrs[i].type_for == type && vm->type_managers.mgrs[i].funs[name] != NULL) { 
             vm->type_managers.mgrs[i].funs[name](vm, object); 
@@ -170,92 +170,92 @@ void pk_run_typefun(pk_vm* vm, pk_type type, pk_object* object, int name) {
     }
 }
 
-pk_object* pk_alloc_object(pk_vm* vm, pk_type type) {
-    pk_object* object;
+pik_object* pik_alloc_object(pik_vm* vm, pik_type type) {
+    pik_object* object;
     if (vm->gc.tombstones == NULL) {
-        object = (pk_object*)calloc(1, sizeof(struct pk_object));
+        object = (pik_object*)calloc(1, sizeof(struct pik_object));
         object->gc.next = vm->gc.first;
         vm->gc.first = object;
         vm->gc.num_objects++;
     } else {
         object = vm->gc.tombstones;
-        vm->gc.tombstones = (pk_object*)object->payload.as_pointer;
+        vm->gc.tombstones = (pik_object*)object->payload.as_pointer;
     }
     object->type = type;
     object->gc.refcnt = 1;
-    object->properties = pk_hashmap_new();
-    pk_run_typefun(vm, type, object, PK_INITFUN);
+    object->properties = pik_hashmap_new();
+    pik_run_typefun(vm, type, object, PIK_INITFUN);
     return object;
 }
 
-void pk_finalize(pk_vm* vm, pk_object* object) {
-    pk_type type = object->type;
-    object->type = PK_TYPE_TOMBSTONE;
+void pik_finalize(pik_vm* vm, pik_object* object) {
+    pik_type type = object->type;
+    object->type = PIK_TYPE_TOMBSTONE;
     // Free object-specific stuff
-    pk_run_typefun(vm, type, object, PK_FINALFUN);
+    pik_run_typefun(vm, type, object, PIK_FINALFUN);
     // Free everything else
     object->flags.global = 0;
     object->flags.obj = 0;
     for (size_t i = 0; i < object->proto.len; i++) {
-        pk_decref(vm, object->proto.bases[i]);
+        pik_decref(vm, object->proto.bases[i]);
     }
     free(object->proto.bases);
     object->proto.bases = NULL;
     object->proto.len = 0;
     object->proto.cap = 0;
-    pk_hashmap_destroy(vm, object->properties);
+    pik_hashmap_destroy(vm, object->properties);
     object->properties = NULL;
 }
 
-#define pk_incref(object) ((object)->gc.refcnt++)
+#define pik_incref(object) ((object)->gc.refcnt++)
 
-void pk_decref(pk_vm* vm, pk_object* object) {
+void pik_decref(pik_vm* vm, pik_object* object) {
     if (object == NULL) return;
     object->gc.refcnt--;
     if (object->gc.refcnt == 0) {
         // Free it now, no other references
-        pk_finalize(vm, object);
+        pik_finalize(vm, object);
         // Put it in the tombstone list
         object->payload.as_pointer = (void*)vm->gc.tombstones;
         vm->gc.tombstones = object;
     }
 }
 
-pk_vm* pk_new(void) {
-    pk_vm* vm = (pk_vm*)calloc(1, sizeof(struct pk_vm));
-    vm->global_scope = pk_alloc_object(vm, PK_TYPE_NONE);
+pik_vm* pik_new(void) {
+    pik_vm* vm = (pik_vm*)calloc(1, sizeof(struct pik_vm));
+    vm->global_scope = pik_alloc_object(vm, PIK_TYPE_NONE);
     return vm;
 }
 
 // ------------------------- Hashmap stuff -------------------------
 
-inline pk_hashmap* pk_hashmap_new(void) {
-    return (pk_hashmap*)calloc(1, sizeof(struct pk_hashmap));
+inline pik_hashmap* pik_hashmap_new(void) {
+    return (pik_hashmap*)calloc(1, sizeof(struct pik_hashmap));
 }
 
-unsigned int pk_hashmap_hash(const char* value) {
+unsigned int pik_hashmap_hash(const char* value) {
     unsigned int hash = 5381;
     for (char c = *value; c; c = *value++) {
         hash = (hash << 5) + hash + c;
     }
-    return hash & PK_HASHMAP_BUCKETMASK;
+    return hash & PIK_HASHMAP_BUCKETMASK;
 }
     
-void pk_hashmap_destroy(pk_vm* vm, pk_hashmap* map) {
+void pik_hashmap_destroy(pik_vm* vm, pik_hashmap* map) {
     if (map == NULL) return;
-    for (size_t b = 0; b < PK_HASHMAP_BUCKETS; b++) {
-        pk_hashbucket bucket = map->buckets[b];
+    for (size_t b = 0; b < PIK_HASHMAP_BUCKETS; b++) {
+        pik_hashbucket bucket = map->buckets[b];
         for (size_t e = 0; e < bucket.cap; e++) {
             free(bucket.entries[e].key);
-            pk_decref(vm, bucket.entries[e].value);
+            pik_decref(vm, bucket.entries[e].value);
         }
     }
     free(map);
 }
     
-void pk_hashmap_put(pk_hashmap* map, const char* key, pk_object* value, bool readonly) {
-    pk_incref(value);
-    pk_hashbucket b = map->buckets[pk_hashmap_hash(key)];
+void pik_hashmap_put(pik_hashmap* map, const char* key, pik_object* value, bool readonly) {
+    pik_incref(value);
+    pik_hashbucket b = map->buckets[pik_hashmap_hash(key)];
     for (size_t i = 0; i < b.cap; i++) {
         if (streq(b.entries[i].key, key)) {
             b.entries[i].value = value;
@@ -263,15 +263,15 @@ void pk_hashmap_put(pk_hashmap* map, const char* key, pk_object* value, bool rea
             return;
         }
     }
-    b.entries = (pk_hashentry*)realloc(b.entries, sizeof(struct pk_hashentry) * (b.cap + 1));
+    b.entries = (pik_hashentry*)realloc(b.entries, sizeof(struct pik_hashentry) * (b.cap + 1));
     b.entries[b.cap].key = strdup(key);
     b.entries[b.cap].value = value;
     b.cap++;
 }
 
-#ifdef PK_DEBUG
+#ifdef PIK_DEBUG
 int main(void) {
-    printf("%lu %lu %lu\nfoobarbaz", sizeof(struct pk_object), sizeof(struct pk_vm), sizeof(struct pk_hashmap));
+    printf("%lu %lu %lu\nfoobarbaz", sizeof(struct pik_object), sizeof(struct pik_vm), sizeof(struct pik_hashmap));
     return 0;
 }
 #endif
