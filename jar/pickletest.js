f644eff207f401d2d925702bf7888c7279c961c8
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-04-19 11:50:49 -0400

    add output test

--- jar/pickletest.js
+++ jar/pickletest.js
@@ -1,2 +1,10 @@
+const $ = s => document.querySelector(s);
+
 ace.config.set('basePath', 'https://cdn.jsdelivr.net/npm/ace-builds@1.10.0/src-noconflict/');
 var editor = ace.edit("textarea");
+
+function output(x) {
+    $("#output").innerHTML += x;
+}
+
+output("testing 123");
