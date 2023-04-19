2185a9b0a4d6bd7c3aafd8f05ee51f444fc4bb7b
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-04-19 16:30:39 +0000

    remove output panel to see if it broke anything

--- jar/pickletest.js
+++ jar/pickletest.js
@@ -1,6 +1,6 @@
 const $ = s => document.querySelector(s);
 
-//ace.config.set('basePath', 'https://cdn.jsdelivr.net/npm/ace-builds@1.10.0/src-noconflict/');
+ace.config.set('basePath', 'https://cdn.jsdelivr.net/npm/ace-builds@1.10.0/src-noconflict/');
 var editor = ace.edit("picklecode");
 
 function output(x) {
