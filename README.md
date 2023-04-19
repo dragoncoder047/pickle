1457b3affbb7b8c6b756f8386ee9f5af5ff16ed9
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-04-19 18:12:04 +0000

    rename to seeder

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 It's Python! No, it's Tcl!, Wait, what is it? It's a programming language!
 
-The test arena (which I have named the "jar" for no reason) can be found at <https://dragoncoder047.github.io/pickle/jar/jar.html>.
+The test arena (which I have named the "seeder" for no reason) can be found at <https://dragoncoder047.github.io/pickle/seeder/seeder.html>.
--- seeder/seeder.js
+++ seeder/seeder.js
@@ -17,9 +17,13 @@ for (var i = 0; i < 10; i++) output("Test output appears here\n");
 editor.getSession().on('change', () => {
     try {
         var text = editor.getValue();
-        var parseResult = pickleParse(text);
+        var t = new PickleTokenizer(text);
+        var tokens = [];
+        while (!t.empty()) {
+            tokens.push(t.nextToken());
+        }
         clearOutput();
-        output(JSON.stringify(parseResult, null, 4));
+        output(JSON.stringify(tokens, null, 4));
     } catch (e) {
         output(`<span class="error">${e}\n${e.stack}</span>`)
     }
