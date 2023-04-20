57712b308a5699a082e1749ed606d6da7322d02c
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-04-20 14:00:49 +0000

    try to get rid of errors

--- seeder/seeder.js
+++ seeder/seeder.js
@@ -27,12 +27,14 @@ console.debug = function patched(...args) {
 var tokenizer = null;
 var annotations = [];
 var animationID = undefined;
+var gotErrors = false;
 function tokenizeAnimation() {
     try {
         if (!tokenizer.done()) {
             var tok = tokenizer.nextToken();
             if (tok) {
                 if (tok.type == "error") {
+                    gotErrors = true;
                     annotations.push({
                         row: tok.start.line - 1,
                         column: tok.start.col,
@@ -44,6 +46,11 @@ function tokenizeAnimation() {
                 output(`[${tok.start.line}:${tok.start.col} - ${tok.end.line}:${tok.end.col}]\t${[tok.type].concat(tok.subtypes).join(".")}\t${JSON.stringify(tok.content)}\t${tok.message}\n`);
             }
             animationID = requestAnimationFrame(tokenizeAnimation);
+        } else {
+            if (!gotErrors) {
+                editor.getSession().setAnnotations([]);
+                annotations = [];
+            }
         }
     } catch (e) {
         output(`<span class="outerror">${e}\n${e.stack}</span>`)
@@ -55,6 +62,7 @@ editor.getSession().on('change', () => {
     if (animationID) cancelAnimationFrame(animationID);
     tokenizer = new PickleTokenizer(editor.getValue());
     annotations = [];
+    gotErrors = false;
     clearOutput();
     tokenizeAnimation();
 });
