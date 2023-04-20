f23cb5604074eb4a596548e781af8734afd9a76b
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-04-20 12:12:23 +0000

    move string up first to try to make it run faster

--- pickle.js
+++ pickle.js
@@ -154,13 +154,13 @@ class PickleTokenizer {
             { type: "paren", re: /^[\(\)\[\]]/, significant: true, groupNum: 0 },
             { type: "space", re: /^(?!\n)\s+/, significant: false },
             { type: "eol", re: /^[;\n]/, significant: true, groupNum: 0 },
+            { type: "string.quote", re: /^(["'])((\\.|(?!\1)[^\\])*)\1/, significant: true, groupNum: 2 },
             { type: "singleton", re: /^(true|false|nil)/, significant: true, groupNum: 0 },
             { type: "number.complex", re: /^-?[0-9]+(\.[0-9]+)?e[+-]\d+[+-][0-9]+(\.[0-9]+)?e[+-]\d+j/, significant: true, groupNum: 0 },
             { type: "number.rational", re: /^-?[0-9]+\/[0-9]+/, significant: true, groupNum: 0 },
             { type: "number.float", re: /^-?[0-9]+(\.[0-9]+)?(e[+-]\d+)?/i, significant: true, groupNum: 0 },
             { type: "number.integer", re: /^-?([1-9][0-9]*|0x[0-9a-f]+|0b[01]+)/i, significant: true, groupNum: 0 },
             { type: "symbol", re: /^[a-z_][a-z0-9_]*\??/i, significant: true, groupNum: 0 },
-            { type: "string.quote", re: /^(["'])((\\.|(?!\1)[^\\])*)\1/, significant: true, groupNum: 2 },
             { type: "operator", re: /^[-~`!@$%^&*_+=[\]|\\:<>,.?/]*/, significant: true, groupNum: 0 },
         ]
         for (var { type, re, significant, groupNum } of TOKEN_REGEXES) {
