3a8cca9703b37079f2ec9e386ac2baf7524f6f90
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-06-26 19:03:08 +0000

    port some of the JS code to C++ [skip actions]

--- cpp/pickle.cpp
+++ cpp/pickle.cpp
@@ -2,6 +2,82 @@
 
 namespace pickle {
 
+char unescape(char c) {
+    switch (c) {
+        case 'b': return '\b';
+        case 't': return '\t';
+        case 'n': return '\n';
+        case 'v': return '\v';
+        case 'f': return '\f';
+        case 'r': return '\r';
+        case 'a': return '\a';
+        case 'o': return '{';
+        case 'c': return '}';
+        case '\n': return 0;
+        default: return c;
+    }
+}
 
+char escape(char c) {
+    switch (c) {
+        case '\b': return 'b';
+        case '\t': return 't';
+        case '\n': return 'n';
+        case '\v': return 'v';
+        case '\f': return 'f';
+        case '\r': return 'r';
+        case '\a': return 'a';
+        case '{': return 'o';
+        case '}': return 'c';
+        default: return c;
+    }
+}
+
+bool needs_escape(char c) {
+    return strchr("{}\b\t\n\v\f\r\a\\\"", c) != NULL;
+}
+
+loc::loc(size_t line, size_t col)
+: line(line),
+  col(col) {};
+
+token::token(token_type type, char* content, loc start, loc end, char* filename, char* message)
+: type(type),
+  content(content ? strdup(content) : NULL),
+  start(start),
+  end(end),
+  filename(filename ? strdup(filename) : NULL),
+  message(message ? strdup(message) : NULL) {};
+
+token::~token() {
+    free(this->content);
+    free(this->filename);
+    free(this->message);
+}
+
+tokenizer::tokenizer(const char* stream, const char* filename)
+: filename((char*)filename),
+  stream((char*)stream),
+  len(strlen(stream)),
+  bi(0),
+  i(0),
+  last_token(NULL),
+  buffer(NULL),
+  bufsz(0),
+  buflen(0) {};
+
+
+token* tokenizer::error_token(char* message) {
+    if (this->bi == this->i) this->i++;
+    char* message2;
+    if (message) message2 = strdup(message);
+    else {
+        char c = this->at(-1);
+        asprintf(&message2, "unexpected '%s%c'", needs_escape(c) ? "\\" : "", escape(c));
+    }
+    token* foo = this->make_token(ERROR, message2);
+    free(message2);
+    return foo;
+}
 
 }
--- cpp/pickle.hpp
+++ cpp/pickle.hpp
@@ -1,11 +1,92 @@
 #pragma once
 
 #include "tinobsy/tinobsy.hpp"
+#include <cstdlib>
+#include <cctype>
+#include <cstdint>
+#include <cstring>
+#include <csetjmp>
 
 namespace pickle {
 
+char escape(char c);
+char unescape(char c);
+bool needs_escape(char c);
+
+class loc {
+    loc(size_t line, size_t col);
+    public:
+    size_t line;
+    size_t col;
+    friend class tokenizer;
+    friend class token;
+};
+
+typedef enum {
+    ERROR,
+    STRING,
+    PAREN,
+    EOL,
+    SYMBOL
+} token_type;
+
+class token {
+    token(token_type type, char* content, loc start, loc end, char* filename, char* message);
+
+    public:
+    token_type type;
+    char* content;
+    loc start;
+    loc end;
+    char* filename;
+    char* message;
+
+    ~token();
+
+    friend class tokenizer;
+};
+
 // Tokenizer
-class 
+class tokenizer {
+    size_t bi;
+    size_t i;
+    size_t len;
+    token* last_token;
+    char* buffer;
+    size_t bufsz;
+    size_t buflen;
+    void append_to_buffer(char c);
+
+    token* error_token(char* message = NULL);
+    token* make_token(token_type type, char* message);
+    loc current_loc();
+    bool test_str(char* what);
+    bool test_any(char* what);
+    bool test(char what);
+    bool done();
+    void advance(ssize_t i = 1);
+    char at(ssize_t i = 0);
+    char* str_at(ssize_t i = 0);
+
+    bool try_colon_block();
+    bool try_block_comment();
+    bool try_line_comment();
+    bool try_paren();
+    bool try_space();
+    bool try_eol();
+    bool try_symbol();
+    bool try_curly_string();
+    bool try_quote_string();
+
+    public:
+    char* filename;
+    char* stream;
+
+    tokenizer(const char* stream, const char* filename = "");
+    ~tokenizer();
+
+    token* next_token();
+};
 
 
 
