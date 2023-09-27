380f4f7780a8138173593e05696d002a3a015a07
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-09-27 11:49:46 -0400

    basic objects

--- cpp/pickle.cpp
+++ cpp/pickle.cpp
@@ -2,6 +2,8 @@
 
 namespace pickle {
 
+using tinobsy::cell;
+
 char unescape(char c) {
     switch (c) {
         case 'b': return '\b';
@@ -37,180 +39,47 @@ bool needs_escape(char c) {
     return strchr("{}\b\t\n\v\f\r\a\\\"", c) != NULL;
 }
 
-location::location()
-: location(1, 1) {}
+location::location() {}
 
 location::location(size_t line, size_t col)
 : line(line),
   col(col) {}
 
-token::token(token_type type, char* content, location start, location end, char* filename, const char* message)
-: type(type),
-  content(content ? strdup(content) : NULL),
-  start(start),
-  end(end),
-  filename(filename ? strdup(filename) : NULL),
-  message(message ? strdup(message) : NULL) {}
-
-token::~token() {
-    free(this->content);
-    free(this->filename);
-    free(this->message);
+static void init_metadata(object* self, va_list args) {
+    self->cells = new cell[2];
+    self->cells[0].as_ptr = (void*)(new location(va_arg(args, location*)));
+    self->cells[1].as_obj = va_arg(args, object*);
 }
 
-tokenizer::tokenizer(const char* stream, const char* filename, location offset)
-: offset(offset),
-  filename((char*)filename),
-  stream((char*)stream),
-  len(strlen(stream)),
-  bi(0),
-  i(0),
-  last_token(NULL),
-  buffer(NULL),
-  bufsz(0),
-  buflen(0) {}
-
-tokenizer::~tokenizer() {
-    free(this->buffer);
+static void mark_metadata(object* self) {
+    self->cells[1].as_obj->mark();
 }
 
-char tokenizer::at(ssize_t i) {
-    ssize_t off = i + this->i;
-    if (off < 0 || off >= this->len) return EOF;
-    return this->stream[off];
+static void finalize_metadata(object* self) {
+    delete (location*)(self->cells[0].as_ptr);
 }
 
-char* tokenizer::str_at(ssize_t i) {
-    ssize_t off = i + this->i;
-    if (off < 0 || off >= this->len) return NULL;
-    return &this->stream[off];
+static void init_c_function(object* self, va_list args) {
+    self->as_ptr = (void*)va_arg(args, func_ptr);
 }
 
-[[noreturn]] void tokenizer::error(char* offending, const char* message) {
-    if (this->bi == this->i) this->i++;
-    char m2[17];
-    if (!message) {
-        char c = this->at(-1);
-        asprintf((char**)&m2, "unexpected '%s%c'", needs_escape(c) ? "\\" : "", escape(c));
-        message = (const char*) m2;
-    }
-    this->got_token(ERROR, offending, message, SIGNIFICANT);
+static void init_function_partial(object* self, va_list args) {
+    self->cells = new cell[5];
+    self->cells[0].as_obj = va_arg(args, object*); // function
+    self->cells[1].as_obj = va_arg(args, object*); // args
+    self->cells[2].as_obj = va_arg(args, object*); // env
+    self->cells[3].as_obj = va_arg(args, object*); // cont
+    self->cells[4].as_obj = va_arg(args, object*); // failcont
 }
 
-[[noreturn]] void tokenizer::got_token(token_type type, char* content, const char* message, significance significant, bool free_content) {
-    if (content == NULL) {
-        int len = this->i - this->bi;
-        asprintf(&content, "%.*s", len, &this->stream[this->bi]);
-    }
-    location here, start;
-    for (size_t i = 0; i < this->i; i++) {
-        char c = this->stream[i];
-        if (i == this->bi) start = here;
-        if (c == '\n') {
-            here.line++;
-            here.col = 1;
-        } else {
-            here.col++;
-        }
-    }
-    here.line += this->offset.line;
-    here.col += this->offset.col;
-    start.line += this->offset.line;
-    start.col += this->offset.col;
-    this->last_token = new token(type, content, start, here, this->filename, message);
-    if (content == this->buffer) free(this->buffer), this->bufsz = this->buflen = 0, this->buffer = NULL;
-    else if (free_content && content) free(content);
-    longjmp(this->success, (int)significant);
+static void mark_function_partial(object* self) {
+    for (int i = 0; i < 5; i++) self->cells[i].as_obj->mark();
 }
 
-size_t tokenizer::save() {
-    return this->i;
-}
-
-void tokenizer::restore(size_t where) {
-    this->i = where;
-}
-
-#define CHUNK_SZ 16
-void tokenizer::append_to_buffer(char c) {
-    this->buflen++;
-    if (this->buflen >= this->bufsz) {
-        this->bufsz += CHUNK_SZ;
-        this->buffer = (char*)realloc(this->buffer, this->bufsz + 1);
-    }
-    this->buffer[this->buflen - 1] = c;
-    this->buffer[this->buflen] = 0;
-}
-
-bool tokenizer::test_str(char* what) {
-    size_t len = strlen(what);
-    bool matched = !strncmp(this->str_at(), what, len);
-    if (matched) this->advance(len);
-    return matched;
-}
-
-bool tokenizer::test_any(char* what) {
-    bool matched = strchr(what, this->at()) != NULL;
-    if (matched) this->advance();
-    return matched;
-}
-
-bool tokenizer::test(char what) {
-    bool matched = this->at() == what;
-    if (matched) this->advance();
-    return matched;
-}
-
-bool tokenizer::test(bool (*match)(char)) {
-    bool matched = match(this->at());
-    if (matched) this->advance();
-    return matched;
-}
-
-bool tokenizer::test(int (*match)(int)) {
-    bool matched = match(this->at());
-    if (matched) this->advance();
-    return matched;
-}
-
-bool tokenizer::done() {
-    return this->i >= this->len;
-}
-
-void tokenizer::advance(ssize_t i) {
-    size_t newi = this->i + i;
-    if (newi > 0 && newi <= this->len) this->i = newi;
-}
-
-void tokenizer::try_colon_block() {
-    size_t start = this->save();
-    // Abort if there is no colon
-    if (!this->test(':')) return;
-    while (this->test([] (char what) -> bool { return isspace(what) && what != '\n'; }));
-    if (this->done() || this->at() != '\n') { this->restore(start); return; }
-    // Get the indent
-    size_t in_start = this->save();
-    int in_len = 0;
-    while (this->test(isspace)) in_len++;
-    // Make sure there actually is an indent
-    if (!len) this->error(NULL, "expected indent after colon+newline");
-    char* indent;
-    asprintf(&indent, "%.*s", in_len, &this->stream[in_start]);
-    // Ensure indent is all the same character
-    char ex_indent = indent[0];
-    for (size_t i = 0; i < in_len; i++) if (indent[i] != ex_indent) this->error(indent, "mix of tabs and spaces indenting block");
-    // Get the lines
-    while (true) {
-        // Get one line
-        do this->append_to_buffer(this->at()), this->advance(); while (this->at() != '\n');
-        // At beg of next line: check to see if there is an unindent
-        if (!this->test_str(indent)) {
-            free(indent);
-            if (isspace(this->at())) this->error(NULL, "unindent does not match previous indent");
-            break;
-        }
-    }
-    this->got_token(STRING, this->buffer);
-}
+const object_schema metadata_type("object_metadata", init_metadata, NULL, mark_metadata, finalize_metadata);
+const object_schema cons_type("cons", tinobsy::schema_functions::init_cons, NULL, tinobsy::schema_functions::mark_cons, tinobsy::schema_functions::finalize_cons);
+const object_schema partial_type("function_partial", init_function_partial, NULL, mark_metadata, NULL);
+const object_schema c_function_type("c_function", init_c_function, NULL, NULL, NULL);
+const object_schema string_type("string", tinobsy::schema_functions::init_str, tinobsy::schema_functions::cmp_str, NULL, tinobsy::schema_functions::finalize_str);
 
 }
--- cpp/pickle.hpp
+++ cpp/pickle.hpp
@@ -1,13 +1,16 @@
-#pragma once
+#ifndef PICKLE_H
+#define PICKLE_H
 
 #include "tinobsy/tinobsy.hpp"
 #include <cstdlib>
 #include <cctype>
 #include <cstdint>
 #include <cstring>
-#include <csetjmp>
+#include <cstdarg>
 
 namespace pickle {
+
+using tinobsy::object, tinobsy::object_schema;
 
 char escape(char c);
 char unescape(char c);
@@ -18,120 +21,26 @@ class location {
     public:
     location();
     location(size_t line, size_t col);
-    size_t line;
-    size_t col;
+    size_t line = 1;
+    size_t col = 1;
 };
 
-// What kind of token it is.
-typedef enum {
-    ERROR,
-    STRING,
-    PAREN,
-    EOL,
-    SYMBOL,
-} token_type;
+class pickle : public tinobsy::vm;
 
-// A struct to hold the data for tokens.
-class token {
-    token(token_type type, char* content, location start, location end, char* filename, const char* message);
-
-    public:
-    token_type type;
-    char* content;
-    location start;
-    location end;
-    char* filename;
-    char* message;
-
-    ~token();
-
-    friend class tokenizer;
-};
-
-// Whether the token means anything to the parser or not. If it doesn't, the token is dropped.
-typedef enum {
-    SIGNIFICANT = 1,
-    INSIGNIFICANT = 2
-} significance;
-
-// Tokenizer
-class tokenizer {
-    // If not line 1, col 1, where in the file parsing started. (Used for nested blocks).
-    location offset;
-    // Begin index
-    size_t bi;
-    // Current index
-    size_t i;
-    // Total length of stream to be parsed
-    size_t len;
-    // Last token successfully parsed
-    token* last_token;
-    // Buffer for string data not taken directly from the source stream.
-    char* buffer;
-    // Capacity of the buffer.
-    size_t bufsz;
-    // Number of characters in the buffer.
-    size_t buflen;
-    // Append the character to the buffer, enlarging the buffer if needed.
-    void append_to_buffer(char c);
-
-    // Used to longjmp back to the top when a token is successfully parsed.
-    jmp_buf success;
-
-    // Returns the current index.
-    size_t save();
-    // Restores the current index to what it was previously.
-    void restore(size_t where);
-
-    // Signals an error-token.
-    [[noreturn]] void error(char* offending, const char* message);
-
-    // Signals a good token.
-    [[noreturn]] void got_token(token_type type, char* content, const char* message = NULL, significance significant = SIGNIFICANT, bool free_content = true);
-
-    // If the current position begins with `what`, advances and returns true, otherwise returns false.
-    bool test_str(char* what);
-    // If the current position begins with `what`, advances and returns true, otherwise returns false.
-    bool test_any(char* what);
-    // If the current position begins with `what`, advances and returns true, otherwise returns false.
-    bool test(char what);
-    // If `match` returns true for the current character, advances and returns true, otherwise returns false.
-    bool test(bool (*match)(char));
-    // If `match` returns true for the current character, advances and returns true, otherwise returns false.
-    bool test(int (*match)(int));
-    // True if all the characters in the stream have been parsed.
-    bool done();
-    // Advances the pointer by `i` characters.
-    void advance(ssize_t i = 1);
-    // Returns the character at the current pointer +- an offset.
-    char at(ssize_t i = 0);
-    // Returns the remainder of the string starting at the current pointer +- an offset.
-    char* str_at(ssize_t i = 0);
-
-    void try_colon_block();
-    void try_block_comment();
-    void try_line_comment();
-    void try_paren();
-    void try_space();
-    void try_eol();
-    void try_symbol();
-    void try_curly_string();
-    void try_quote_string();
-
-    public:
-    // The file that the stream originated from.
-    char* filename;
-    // The contents to be parsed.
-    char* stream;
-
-    tokenizer(const char* stream, const char* filename = "", location offset = {0, 0});
-    ~tokenizer();
-
-    token* next_token();
-};
+typedef void (*func_ptr)(pickle* runner, object* args, object* env, object* cont, object* fail_cont);
 
 
+extern const object_schema metadata_type;
+extern const object_schema cons_type;
+extern const object_schema partial_type;
+extern const object_schema c_function_type;
+extern const object_schema string_type;
 
 }
 
+#define car(x) ((x)->cells[0].as_obj)
+#define cdr(x) ((x)->cells[1].as_obj)
+
 #include "pickle.cpp"
+
+#endif
